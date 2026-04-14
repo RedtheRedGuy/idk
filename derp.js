367188f24a67fa097697f13beeacd2cebc12193f
RedtheRedGuy <186442544+RedtheRedGuy@users.noreply.github.com>
2026-04-14 11:58:51 +0700

    Update derp.js

--- derp.js
+++ derp.js
@@ -1,8 +1,8 @@
-
 elements.derp = {
     color: "#ff0000",
     category: "life",
     state: "solid",
+    density: 1000,
 
     behavior: [
         "XX|XX|XX",
@@ -11,12 +11,20 @@ elements.derp = {
     ],
 
     tick: function(pixel) {
+
         // gravity
         if (isEmpty(pixel.x, pixel.y+1)) {
             movePixel(pixel, pixel.x, pixel.y+1);
+            return;
         }
 
-        // immortal
+        // small random movement
+        let dir = Math.random() < 0.5 ? -1 : 1;
+        if (isEmpty(pixel.x+dir, pixel.y)) {
+            movePixel(pixel, pixel.x+dir, pixel.y);
+        }
+
+        // IMMORTAL
         pixel.dead = false;
         pixel.burning = false;
         pixel.temp = 20;
